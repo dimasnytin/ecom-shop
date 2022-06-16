a491f19192579be81bda733428f3a34a06d467bd
Dima Snytin <dimasnytin@gmail.com>
2022-06-16 14:52:24 +0300

    add mobile menu and button menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,26 @@
 'use strict';
-// модальное окно подписки
+
+// modal variables
 const modal = document.querySelector('[data-modal]');
-const modalCloseBtn = document.querySelector('[data-model-close]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
 const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
 
 // modal function
-const modalCloseFunc = function () {
-  modal.classList.add('closed')
-}
+const modalCloseFunc = function () { modal.classList.add('closed') }
 
 // modal eventListener
 modalCloseOverlay.addEventListener('click', modalCloseFunc);
 modalCloseBtn.addEventListener('click', modalCloseFunc);
 
 
-// toast variables
+
+
+
+// notification toast variables
 const notificationToast = document.querySelector('[data-toast]');
 const toastCloseBtn = document.querySelector('[data-toast-close]');
 
-// toast eventListener
-toastCloseBtn.addEventListener('click', function() {
+// notification toast eventListener
+toastCloseBtn.addEventListener('click', function () {
   notificationToast.classList.add('closed');
-})
+});
